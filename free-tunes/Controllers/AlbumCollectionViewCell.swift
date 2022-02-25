c2a76833c08026d14a0cdeddd3916a4925e3a23d
Tshwarelo Mafaralala <tmafaralala@DVTL102833.local>
2022-02-25 10:29:06 +0200

    created collection view template cell class

---
+++ free-tunes/Controllers/AlbumCollectionViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AlbumCollectionViewCell.swift
+//  free-tunes
+//
+//  Created by Tshwarelo Mafaralala on 2022/02/25.
+//
+
+import UIKit
+
+class AlbumCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var albumTitle: UILabel!
+    @IBOutlet weak var albumCover: UIImageView!
+    
+    func designCell() {
+        albumCover.layer.cornerRadius = 13.0
+        albumCover.clipsToBounds = true
+        albumCover.layer.shadowColor = UIColor.darkGray.cgColor
+        albumCover.layer.shadowOffset = CGSize(width: 20.0, height: 20.0)
+        albumCover.layer.shadowRadius = 13.0
+        albumCover.layer.shadowOpacity = 0.9
+    }
+}
