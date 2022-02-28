fc862315dab6256e90fad38ddaf4a17b94da26b7
Tshwarelo Mafaralala <tmafaralala@DVTL102833.local>
2022-02-28 12:56:02 +0200

    login view controller setup

---
+++ free-tunes/Controllers/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  free-tunes
+//
+//  Created by Tshwarelo Mafaralala on 2022/02/28.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var headerText: UILabel!
+    @IBOutlet weak var usernameInput: UITextField!
+    @IBOutlet weak var passwordInput: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
