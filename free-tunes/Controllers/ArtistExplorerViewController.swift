7a54e8de44d98b3d33e2e01a5ecefb239d41949d
tmafaralala <tmafaralala@jhb.dvt.co.za>
2022-03-13 19:21:24 +0200

    created custom collection view cell

--- free-tunes/Controllers/ArtistExplorerViewController.swift
+++ free-tunes/Controllers/ArtistExplorerViewController.swift
@@ -6,7 +6,8 @@
 //
 
 import UIKit
-let authManager = AuthController()
+import AVFoundation
+
 class ArtistExplorerViewController: UIViewController {
 
     @IBOutlet weak var artistCollectionView: UICollectionView!
@@ -63,3 +64,72 @@ extension ArtistExplorerViewController: UICollectionViewDelegate, UICollectionVi
         }
     }
 }
+
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myCollectionView: UICollectionView!
+    var looper: AVPlayerLooper!
+    var player: AVQueuePlayer!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        myCollectionView.register(UINib(nibName: "MyCollectionViewCell", bundle: .main), forCellWithReuseIdentifier: "MyCollectionViewCell")
+        myCollectionView.dataSource = self
+        myCollectionView.delegate = self
+        myCollectionView.isPagingEnabled = true
+    }
+
+    func loadRadio(radioURL: String) {
+
+            guard let url = URL.init(string: radioURL) else { return }
+            let playerItem = AVPlayerItem.init(url: url)
+            player = AVQueuePlayer(playerItem: playerItem)
+            looper = AVPlayerLooper(player: player, templateItem: playerItem)
+            player?.play()
+        }
+
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        myCollectionView.collectionViewLayout.invalidateLayout()
+    }
+
+
+}
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = myCollectionView.dequeueReusableCell(withReuseIdentifier: "MyCollectionViewCell", for: indexPath) as? CollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        cell.cellImage.image = UIImage(named: "500x500")
+        cell.trackName.text = "Abalele"
+        cell.artistName.text = "Kabza de small"
+        cell.styleLikeIcon()
+        cell.styleShareIcon()
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        looper = nil
+        loadRadio(radioURL: "https://cdns-preview-3.dzcdn.net/stream/c-34f6682187fd47601e177f80e33cb38a-3.mp3")
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if player.volume != 0 {
+            player.volume = 0
+            return
+        }
+        player.volume = 1.0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+      return CGSize(width: view.frame.width, height: view.bounds.height)
+        
+    }
+
+}
+
---
+++ free-tunes/Controllers/TrackCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TrackCollectionViewCell.swift
+//  free-tunes
+//
+//  Created by Tshwarelo Mafaralala on 2022/03/13.
+//
+
+import UIKit
+
+class TrackCollectionViewCell: UICollectionViewCell {
+
+
+}
